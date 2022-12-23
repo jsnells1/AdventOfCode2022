700c8e4a25987e929e51d54b4ee3c1716d3c9bf4
jsnells1 <speedwiseowns@hotmail.com>
2022-12-22 21:59:54 -0500

    Adds Day4 Part 1

---
+++ src/main/java/org/jsnells/day/Day4.java
@@ -0,0 +1,49 @@
+package org.jsnells.day;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class Day4 extends Day {
+    @Override
+    public Object partOne() {
+        var sections = this.getInputText().split(NEW_LINE_SEPARATOR);
+
+        int count = 0;
+        for (var section : sections) {
+            var ranges = section.split(",");
+            var range1 = new Range(ranges[0]);
+            var range2 = new Range(ranges[1]);
+
+            if (range1.contains(range2) || range2.contains(range1)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    @Override
+    public Object partTwo() {
+        return "";
+    }
+
+    private static class Range {
+        private final int min;
+        private final int max;
+
+        public Range(String range) {
+            var split = range.split("-");
+
+            min = Integer.parseInt(split[0]);
+            max = Integer.parseInt(split[1]);
+        }
+
+        public boolean contains(Range range) {
+            return this.max >= range.max && this.min <= range.min;
+        }
+
+        public int size() {
+            return max - min;
+        }
+    }
+}
