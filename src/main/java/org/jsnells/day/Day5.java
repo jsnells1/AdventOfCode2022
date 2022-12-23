e5fbd20c04968fdb1de784448057b169f88b3def
jsnells1 <speedwiseowns@hotmail.com>
2022-12-23 00:13:54 -0500

    Adds Day5 Part 2

--- src/main/java/org/jsnells/day/Day5.java
+++ src/main/java/org/jsnells/day/Day5.java
@@ -46,6 +46,41 @@ public class Day5 extends Day {
 
     @Override
     public Object partTwo() {
-        return "";
+        var inputs = this.getInputText().split(EMPTY_LINE_SEPARATOR);
+        var startingStack = inputs[0].split(NEW_LINE_SEPARATOR);
+
+        List<Stack<String>> stacks = new ArrayList<>();
+        for (int i = 0; i < (startingStack[0].length() + 1) / 4; i++) {
+            stacks.add(new Stack<>());
+        }
+
+        for (int i = startingStack.length - 2; i >= 0; i--) {
+            for (int j = 1, k = 0; j <= startingStack[i].length(); j += 4, k++) {
+                var c = startingStack[i].charAt(j);
+
+                if (c != ' ') {
+                    stacks.get(k).push(c + "");
+                }
+            }
+        }
+
+        for (var instruction : inputs[1].split(NEW_LINE_SEPARATOR)) {
+            var split = instruction.split(" ");
+
+            var amount = Integer.parseInt(split[1]);
+            var from = stacks.get(Integer.parseInt(split[3]) - 1);
+            var to = stacks.get(Integer.parseInt(split[5]) - 1);
+
+            var temp = new Stack<String>();
+            for (int i = 0; i < amount; i++) {
+                temp.push(from.pop());
+            }
+
+            while (!temp.empty()) {
+                to.push(temp.pop());
+            }
+        }
+
+        return stacks.stream().map(Stack::pop).collect(Collectors.joining());
     }
 }
